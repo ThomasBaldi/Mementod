f9b0c13b0c155b6ba81f560cc263a905b7cd4e13
Thomas Baldi <baldithomas@hotmail.it>
2023-06-28 00:21:13 +0200

    fixed user card

--- client/src/components/Profile/User/index.js
+++ client/src/components/Profile/User/index.js
@@ -1,8 +1,24 @@
 import './index.css';
 import { useAuth0 } from '@auth0/auth0-react';
-import { Container } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
+import { Box, useMediaQuery } from '@mui/material';
 
 export default function UserComponent() {
+	const theme = useTheme();
+	const isSmScreen = useMediaQuery(theme.breakpoints.down('sm'));
+	const isMdScreen = useMediaQuery(theme.breakpoints.between('md', 'lg'));
+	const isLgScreen = useMediaQuery(theme.breakpoints.up('lg'));
+
+	let width = 'lg';
+
+	if (isSmScreen) {
+		width = 'sm';
+	} else if (isMdScreen) {
+		width = 'md';
+	} else if (isLgScreen) {
+		width = 'lg';
+	}
+
 	const { user, isAuthenticated, isLoading } = useAuth0();
 
 	if (isLoading) {
@@ -12,13 +28,14 @@ export default function UserComponent() {
 	if (isAuthenticated) {
 		return (
 			<>
-				<Container maxWidth='lg' id='userContainer'>
+				<Box size={width} id='userBox'>
+					<img className='picture' alt='profilePicture' src={user.picture} />
 					<div className='details'>
 						<h3 className='name'>{user.name}</h3>
 						<h3 className='email'>{user.email}</h3>
+						<h3 className='id'>{user.sub}</h3>
 					</div>
-					<img className='picture' alt='profilePicture' src={user.picture} />
-				</Container>
+				</Box>
 			</>
 		);
 	} else {
